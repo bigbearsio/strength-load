a5d5859b33ca24f698b9a52f827f02fa749661f1
Varokas Panusuwan <varokas@panusuwan.com>
2019-06-25 22:33:13 -0700

    Refactor the actions. Add loops

--- README.md
+++ README.md
@@ -12,5 +12,30 @@ Load Testing for Strength project
 sbt gatling:test
 ```
 
+### Example Result
+```
+================================================================================
+---- Global Information --------------------------------------------------------
+> request count                                         30 (OK=24     KO=6     )
+> min response time                                      1 (OK=2      KO=1     )
+> max response time                                     22 (OK=22     KO=12    )
+> mean response time                                    11 (OK=12     KO=6     )
+> std deviation                                          7 (OK=7      KO=4     )
+> response time 50th percentile                          9 (OK=10     KO=6     )
+> response time 75th percentile                         20 (OK=20     KO=10    )
+> response time 95th percentile                         21 (OK=21     KO=12    )
+> response time 99th percentile                         22 (OK=22     KO=12    )
+> mean requests/sec                                     30 (OK=24     KO=6     )
+---- Response Time Distribution ------------------------------------------------
+> t < 800 ms                                            24 ( 80%)
+> 800 ms < t < 1200 ms                                   0 (  0%)
+> t > 1200 ms                                            0 (  0%)
+> failed                                                 6 ( 20%)
+---- Errors --------------------------------------------------------------------
+> status.find.is(200), but actually found 403                         3 (50.00%)
+> jsonPath($.success).find.is(true), but actually found false         3 (50.00%)
+================================================================================
+```
+
 ## Diagram
 ![Diagram](diagram.png)
--- src/test/scala/BearStrengthSimulation.scala
+++ src/test/scala/BearStrengthSimulation.scala
@@ -1,58 +1,77 @@
 import io.gatling.core.Predef.{jsonPath, _}
+import io.gatling.core.action.builder.ActionBuilder
+import io.gatling.core.session.{Expression, Session}
 import io.gatling.http.Predef._
 
 import scala.util.Random
+import scala.concurrent.duration._
+
 
 class BearStrengthSimulation extends Simulation {
 
   val httpProtocol = http
-    .baseUrl("http://localhost:3000") // Here is the root for all relative URLs
+    .baseUrl("http://localhost:3000")
     .header("Content-type","application/json")
     .userAgentHeader("Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:16.0) Gecko/20100101 Firefox/16.0")
 
-  val scn = scenario("Buying Tickets") // A scenario is a chain of requests and pauses
-    //TODO: Loop
+  val scn = scenario("Buying Tickets")
+    .asLongAsDuring(
+      session => !session("hasTicket").asOption[Boolean].getOrElse(false),
+      2 minutes
+    ) {
+      exec( getRemainingSeat() )
+      //TODO: Make randomSeat() exit gracefully if the seatings are exhausted
+      .exec( randomSeat() )
+      .exec( book() )
+      //TODO: Pause with random time - AND/OR -
+      //TODO: Use randomSwitch() to proportinally cancel or drops some of the booked tickets
+      .exec(
+        //TODO: Check if we confirm within a given-time, if not try again
+        //TODO: Apply tolerance to clock skew between gatling and server
+        confirm()
+      )
+      .exitHereIfFailed
+    }
 
-    .exec(
-      http("getRemainingSeats")
+
+  setUp(scn.inject(atOnceUsers(400)).protocols(httpProtocol))
+
+  def getRemainingSeat(): ActionBuilder = {
+    http("getRemainingSeats")
       .get("/remaining")
       .check(
-        status.is(200),
         jsonPath("$.unconfimedTicketsCount").ofType[Int].gte(0).saveAs("unconfimedTicketsCount"),
         jsonPath("$.seats[*]").ofType[String].findAll.saveAs("seats")
       )
-    )
-    .exec( session => {
+  }
+
+  def randomSeat(): Expression[Session] = {
+    session => {
       val seats = session("seats").as[Vector[String]]
       val bookingSeat = seats(Random.nextInt(seats.size))
-      println(bookingSeat)
       session.set("bookingSeat", bookingSeat)
-    })
-    .exec(
-      http("book")
-        .post("/book")
-        .body(StringBody("""{ "seat": "${bookingSeat}" }""")).asJson
-        .check(
-          status.is(200),
-          jsonPath("$.success").ofType[Boolean].is(true),
-          jsonPath("$.seat").ofType[String].is("${bookingSeat}"),
-          jsonPath("$.reserve_expired_time").ofType[Long].saveAs("reserveExpiredTime")
-        )
-    )
-    //TODO: Use randomSwitch() to proportinally cancel or drops some of the booked tickets
-    .exec(
-      //TODO: Check if we confirm within a given-time, if not try again
-      //TODO: Apply tolerance to clock skew between gatling and server
+    }
+  }
 
-      http("confirm")
-        .post("/confirm")
-        .body(StringBody("""{ "seat": "${bookingSeat}" }""")).asJson
-        .check(
-          status.is(200),
-          jsonPath("$.success").ofType[Boolean].is(true),
-          //TODO: Write to log file confirm (clientID, seat, time) for aggregator to check
-        )
-    )
+  def book(): ActionBuilder = {
+    http("book")
+      .post("/book")
+      .body(StringBody("""{ "seat": "${bookingSeat}" }""")).asJson
+      .check(
+        jsonPath("$.success").ofType[Boolean].is(true),
+        jsonPath("$.seat").ofType[String].is("${bookingSeat}"),
+        jsonPath("$.reserve_expired_time").ofType[Long].saveAs("reserveExpiredTime")
+      )
+  }
 
-  setUp(scn.inject(atOnceUsers(1)).protocols(httpProtocol))
+  def confirm(): ActionBuilder = {
+    http("confirm")
+      .post("/confirm")
+      .body(StringBody("""{ "seat": "${bookingSeat}" }""")).asJson
+      .check(
+        jsonPath("$.success").ofType[Boolean].is(true).saveAs("hasTicket"),
+        //TODO: Write to log file confirm (clientID, seat, time) for aggregator to check
+      )
+  }
 }
+
